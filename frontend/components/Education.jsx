725dab188692e44801232ff9d046b436c0f2557e
RujH <rujhaan01@gmail.com>
2020-08-03 15:15:34 -0500

    added globe to education page

--- frontend/components/Education.jsx
+++ frontend/components/Education.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {WebView} from 'react-native-webview';
 export default class Education extends React.Component {
     render () {
         return (
@@ -9,6 +9,12 @@ export default class Education extends React.Component {
                 <Text style= {styles.filler}> </Text>
                 <Text style= {styles.hashtag}> #NameOfHashtag </Text>
                 <Text> num followers</Text>
+                <WebView
+                    source={{html: require('./Globe/globe.js')()}}
+                    originWhitelist={['*']}
+                    style={{height:0, width:300}}
+                />
+
             </View>
         );
     }
