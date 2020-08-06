2fc308165b0a5c86f7b30a6ed0dd5354cc741d90
kimayamcolaco <kimayamcolaco@gmail.com>
2020-08-06 16:48:19 +0530

    Added Impact Page template

---
+++ frontend/components/Impact.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Bottom} from './BottomNavigationBar';
+
+
+  
+
+
+
+export default class Education extends React.Component {
+    render () {
+        return (
+            
+            // change order/styling according to prototype
+            <View style={styles.hello}>
+                <Text style= {styles.filler}> </Text>
+                <Text style= {styles.hashtag}> #NameOfHashtag </Text>
+                <Text> num followers</Text>
+                <Bottom></Bottom>
+                
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    hello: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+    filler: {
+        fontSize: 100,    
+    },
+    hashtag: {
+      fontSize: 22,
+      fontWeight: 'bold',
+    }
+  });
