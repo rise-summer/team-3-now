df9d2fdbda75633271c5cf8bf8c90e7de0e270aa
dnaliu <liudanna17@gmail.com>
2020-08-07 12:14:11 -0400

    updating button

---
+++ frontend/components/HowYouCanHelpButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Button, Layout, Text} from '@ui-kitten/components';
+import {StyleSheet} from 'react-native';;
+export const HelpButton = () => {
+        const [counter, setCounter] = React.useState(0);
+        return (
+            <Layout style={styles.container} level='1'>
+            <Button onPress={() => setCounter(counter + 1)}>
+            How You Can Help
+            </Button>
+            </Layout>
+          );
+        };
+        const styles = StyleSheet.create({
+            container: {
+              flexDirection: 'row',
+              alignItems: 'center',
+              top: -150,
+            },
+            text: {
+              marginHorizontal: 8,
+            },
+          });
